5d043e6011fd64f33cf1cbaa6453d630328d5669
pr0c3r <xfebzx@gmail.com>
2024-05-26 13:53:58 -0300

    implementando o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
        Tabuleiro tab = new Tabuleiro(8, 8);
-       
+       Tela.imprimirTabuleiro(tab);
         Console.ReadLine();
     }
 
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -13,4 +13,7 @@ public class Tabuleiro
    pecas = new Peca[linhas, colunas];
 
   }
+  public Peca peca(int linha, int coluna){
+    return pecas[linha, coluna];
+  }
 }
